befe9b3ba85ae0dcbb259116f204b55159c4770d
VashishattGupta-16 <vassugarg@00786gmail.com>
2026-05-12 20:51:16 +0530

    fix: clean duplicate dependencie

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,7 +2,10 @@ import os
 import multiprocessing
 
 bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"
-workers = multiprocessing.cpu_count() * 2 + 1
+try:
+    workers = max(1, int(os.environ.get("WEB_CONCURRENCY", "")))
+except ValueError:
+    workers = multiprocessing.cpu_count() * 2 + 1
 max_requests = 1000
 max_requests_jitter = 50
 timeout = 120
